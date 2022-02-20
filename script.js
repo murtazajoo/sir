e2f24884b6550252097958889f3069b2d4390779
murtazajoo <98375522+murtazajoo@users.noreply.github.com>
2022-02-20 17:05:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let ham = document.getElementById("ham")
+let links= document.getElementById("link")
+let bar1 = document.getElementById("1bar")
+let bar2= document.getElementById("2bar")
+let bar3= document.getElementById("3bar")
+
+
+
+
+
+ham.addEventListener("click", () => {
+    
+// links.style.transform = "translate(0%)"
+    if (links.style.transform == "translate(0%)") {
+        links.style.transform = "translate(100%)"
+        bar2.style.display = "block"
+        bar1.style.transform = "rotate(0deg)"
+        bar3.style.transform ="rotate(0deg)"
+       
+
+    } else {
+        links.style.transform = "translate(0%)"
+        bar2.style.display = "none"
+        bar1.style.transform = "rotate(145deg)"
+        bar3.style.transform ="rotate(-145deg)"
+        
+
+    }
+    
+
+})
